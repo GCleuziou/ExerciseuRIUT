ad3138a3f3590ba82a66fa8d6d943e48958adcc6
GCleuziou <guillaume.cleuziou@gmail.com>
2021-01-14 11:09:08 +0100

    Test

--- source/TD2/facto.r
+++ source/TD2/facto.r
@@ -8,8 +8,5 @@ tests<-list(
     list("0",reponse=facto(0))
     ,list("1",reponse=facto(1))    
     ,list("2",reponse=facto(2))    
-    ,list("5",reponse=facto(5))    
-    ,list("10",reponse=facto(10))    
-    ,list("37",reponse=facto(37))    
     )
 )
