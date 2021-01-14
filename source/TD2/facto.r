c5413225dffd6fbdb72612a5e2315c5218684910
GCleuziou <guillaume.cleuziou@gmail.com>
2021-01-14 10:56:51 +0100

    Test

--- source/TD2/facto.r
+++ source/TD2/facto.r
@@ -5,12 +5,11 @@ fonctionsAtt<-c("return")
 facto<-function(n){return(prod(1:n)}
 
 tests<-list(
-    list("0",reponse=facto(0)),
-    list("1",reponse=facto(1)),    
-    list("2",reponse=facto(2)),    
-    list("5",reponse=facto(5)),    
-    list("10",reponse=facto(10)),    
-    list("37",reponse=facto(37)),    
+    list("0",reponse=facto(0))
+    ,list("1",reponse=facto(1))    
+    ,list("2",reponse=facto(2))    
+    ,list("5",reponse=facto(5))    
+    ,list("10",reponse=facto(10))    
+    ,list("37",reponse=facto(37))    
     )
 )
-
